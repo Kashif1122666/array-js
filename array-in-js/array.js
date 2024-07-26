26aba63a78f0982de9bc533cfe323c779c650cef
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-07-26 13:58:45 -0700

    Add files via upload
    array-js

---
+++ array-in-js/array.js
@@ -0,0 +1,42 @@
+// CHAPTER-15: ARRAYS
+
+// problem:
+// for multiple values we would have to create multipe variables/containers
+// const fruit0 = "mango";
+// const fruit1 = "banana";
+// const fruit2 = "strawberry";
+
+// solution
+// To solve the above problem, we introduced the concept of array
+// so, array is a container that can store multiple values
+
+// create array
+// const fruits = [];
+// console.log(fruits);
+
+// using fruits name
+// const fruits = ["apple","banana","gavava","mango"];
+// const animals = ["lion","cat","dog","goat"];
+// console.log(fruits);
+// console.log(animals);
+// document.write(animals);
+
+// accessing array-elements, whole array
+// console.log(fruits);
+
+// accessing specific array elements
+// concept of indices (singular : index):
+// const hotelRooms = ["ali","bilal","daniyal","faisal","chodry"];
+// console.log(hotelRooms[2]);
+
+// const animals = ["lion","cat","dog","goat"];
+// console.log(animals[0]);
+
+// mix different types
+// heterogeneous data(mix data-types) vs homogeneous data(same data-type):
+// const mixdata = [1,"hello",true,3.5,"bye"];
+// console.log(mixdata);
+// console.log(mixdata[2]);
+
+// arrays in js are dynamic
+
